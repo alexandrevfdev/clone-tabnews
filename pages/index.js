073d90a29370fa1101b92d9712e479510076be93
Alexandre Fagundes <alexandrevf2021@gmail.com>
2024-12-28 05:29:21 -0300

    page nova

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>A nossa maior glória não reside no fato de nunca cairmos, mas sim em levantarmo-nos sempre depois de cada queda! te amo meu bb </h1>
+    return <h1>A nossa maior glória não reside no fato de nunca cairmos, mas sim em levantarmo-nos sempre depois de cada queda! te amo meu Carmem! 2025 o ano é nosso </h1>
 }
 
 export default  Home;
