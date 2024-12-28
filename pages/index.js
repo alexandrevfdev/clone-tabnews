3cc670b6ccd3974b21467751fc88e38791fe6944
Alexandre Fagundes <alexandrevf2021@gmail.com>
2024-12-28 04:44:12 -0300

    nova frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Sempre pensando positivo</h1>
+    return <h1>A nossa maior glória não reside no fato de nunca cairmos, mas sim em levantarmo-nos sempre depois de cada queda</h1>
 }
 
 export default  Home;
